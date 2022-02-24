1b16e6ff4ce6f49f0016efd1f27cc62765d6c161
brian-mortell <brian.mortell@kellogg.northwestern.edu>
2022-02-24 03:19:31 +0000

    class work

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,15 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
-# 2. decrypt the encrypted string
+secret = "tacos"
+puts secret
+
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
+
+# 2. decrypt the encrypted string (aka give us a way to validate that we know the encryped secret)
+secret_to_test = "tacos"
+result = BCrypt::Password.new(encrypted_secret) == secret_to_test
+puts result
+
 # 3. check if decrypted value matches secret
